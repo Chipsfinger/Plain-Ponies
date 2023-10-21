6e420c9f94e3bdf8bdd8e76ff01f8c5989adb341
Chipsfinger <jacob.e.l.eriksson@gmail.com>
2023-10-21 10:53:55 +0200

    test 3

--- code.js
+++ code.js
@@ -1,21 +1,35 @@
 let loop;
 let fps = 60;
-let cnv, ctx;
+let canvas_, ctx;
 
 window.onload = function () {
         console.log('Gameloop');
+        prepareCanvas();
         loop = setInterval (() =>  {
             update();
             render();
         }, 1000/fps);
 }
 
+function fillCanvas() {
+    ctx.fillStyle = '#1bafdb';
+    ctx.fillRect(0,0, canvas_.width, canvas_.height);
+}
+
+function prepareCanvas() {
+    canvas_ = document.getElementById('canvas_');
+    ctx = canvas_.getContext('2d');
+    document.body.style.padding = 0;
+    document.body.style.margin = 0;
+    canvas_.width = window.innerWidth;
+    canvas_.height = window.innerHeight;
+}
+
 function update () {
     console.log('updating');
-
 }
 
 function render () {
     console.log('rendering');
-
+    fillCanvas ();
 }
